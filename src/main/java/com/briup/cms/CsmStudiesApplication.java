0fcf729ff3cc39918547569356250cc2fc0bbf3d
songyu <1325101650@qq.com>
2019-11-25 17:06:37 +0800

    新增代码

---
+++ src/main/java/com/briup/cms/CsmStudiesApplication.java
@@ -0,0 +1,13 @@
+package com.briup.cms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CsmStudiesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CsmStudiesApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/briup/cms/util/CodeUtil.java
@@ -0,0 +1,9 @@
+package com.briup.cms.util;
+
+public interface CodeUtil {
+    /**
+     * 系统异常状态码
+     */
+    int BAD_CODE = 500;
+
+}
